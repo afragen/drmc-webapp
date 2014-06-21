141bd4d696ecda8dd621862b7eabd12a1ea59580
Andy Fragen <andy@thefragens.com>
2014-06-21 13:48:10 -0700

    fix for adding antibiograms until 2020

--- antibiogram/antibiogram.php
+++ antibiogram/antibiogram.php
@@ -1,23 +1,19 @@
 <?php
+ //libxml_disable_entity_loader( true);
  header('Content-Type: text/html');
  //error_reporting(E_ALL ^ E_NOTICE);
- $y = '';
- if(isset($_GET['year'])) $y = $_GET['year'];
- if ($y > 2011 || $y < 2010) {
-   $current_year=date("Y");
-   $strtotime=strtotime($current_year);
-   $last_year=strtotime("-1 year",$strtotime);
-   $y = date("Y",$last_year);
- }
+ $y = null;
+ if ( isset( $_GET['year'] ) ) { $y = (int) $_GET['year']; }
+ if ($y > 2020 || $y < 2010) { exit; }
+
  $xml = new DOMDocument();
+ if ( isset( $xml->doctype ) ) { exit; }
  $xml->load('./DRMCAntibiogram_' . $y . '.xml');
  
  $xsl = new DOMDocument;
  $xsl->load('../css/antibiogram.xsl');
  
  $proc = new XSLTProcessor();
- $proc->importStyleSheet($xsl);
+ $proc->importStyleSheet( $xsl );
  
- echo $proc->transformToXML($xml);
-
-?>
+ echo $proc->transformToXML( $xml );
