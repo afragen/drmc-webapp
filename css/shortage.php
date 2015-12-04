14a8f89a6ffdd03493b0800fcc73cd718ee78b97
Andy Fragen <andy@thefragens.com>
2015-12-04 10:30:35 -0800

    remove redundent closing tags, add .gitignore

--- css/shortage.php
+++ css/shortage.php
@@ -2,13 +2,11 @@
  header('Content-Type: text/css');
 $xml = new DOMDocument();
 $xml->load('AntibioticShortage.xml');
- 
+
 $xsl = new DOMDocument;
 $xsl->load('shortage.xsl');
- 
+
 $proc = new XSLTProcessor();
 $proc->importStyleSheet($xsl);
- 
-echo $proc->transformToXML($xml);
 
-?>
+echo $proc->transformToXML($xml);
